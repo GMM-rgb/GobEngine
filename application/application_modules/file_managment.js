aa729b9c57af8377062cb26e9411f70b4ebc7a00
Maximus Farvour <maximus.farvour@outlook.com>
2025-12-02 21:30:34 -0800

    Enhance toolbar button functionality and update file management module

--- application/application_modules/file_managment.js
+++ application/application_modules/file_managment.js
@@ -1,9 +1,23 @@
+const Module = require("module");
 const path = require("path");
 const fs = require("fs");
-const Module = require("module");
 
-
+/**
+ * Combine Paths Function to form a single path from multiple segments
+ * @typedef {function(...string): string} CombinePathsFunction
+ * @param  {...string} paths 
+ * @returns {Promise<string>}
+ */
+async function combinePaths(include_current_dir, ...paths) {
+    let CombinedResult = "";
+    if (!paths) return null;
+    if (include_current_dir) {
+        paths.unshift(__dirname);
+    }
+    CombinedResult = path.join(...paths);
+    return CombinedResult !== null ? CombinedResult : null;
+}
 
 module.exports = {
-
+    combinePaths: combinePaths,
 };
