a518dcc27f8b74061e5647a5461284d26faa80d0
Maximus Farvour <maximus.farvour@outlook.com>
2025-12-03 19:45:30 -0800

    Enhance toolbar selection menu functionality with improved animations and styles

--- application/app_contents/app_assets/scripts/toolbar_system.js
+++ application/app_contents/app_assets/scripts/toolbar_system.js
@@ -7,29 +7,25 @@ const toolbarWrapper = document.getElementById("ToolbarWrapper");
  * @returns {Promise<string|boolean?>}
  */
 async function openSelectionMenu(selection_button, status) {
-    if (!selection_button || !status && (typeof status === "boolean" && !(selection_button instanceof HTMLButtonElement))) return null;
+    if (selection_button === null || status === null && (typeof status === "boolean" && !(selection_button instanceof HTMLButtonElement))) return null;
     let TargetSelectionsMenus = document.querySelectorAll(".toolbar-selections-menu");
+
     TargetSelectionsMenus.forEach((supposed_target) => {
-        console.log("hi");
         if (
             supposed_target instanceof HTMLElement &&
             selection_button.getAttribute("id") &&
             supposed_target.getAttribute("name") &&
             selection_button.getAttribute("id").includes(supposed_target.getAttribute("name"))
         ) {
-            console.log("ah");
             if (!status) {
-                if (supposed_target.classList.contains("open")) {
-                    supposed_target.classList.remove("open");
-                    console.log("closed");
-                }
+                supposed_target.classList.remove("open");
+                console.log("closed");
             } else if (status) {
-                if (!supposed_target.classList.contains("open")) {
-                    supposed_target.classList.add("open");
-                    console.log("opened");
-                }
+                supposed_target.classList.add("open");
+                console.log("opened");
             }
         } else return null;
     });
+
     return (selection_button, status);
 }
